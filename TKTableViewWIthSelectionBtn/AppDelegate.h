a44c2602a069c290f7323f09260817af41ddf9d2
Tushar Kolhe <tusharkolhe@tushars-macbook-air.local>
2018-04-15 09:12:20 +0530

    Initial Commit

---
+++ TKTableViewWIthSelectionBtn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TKTableViewWIthSelectionBtn
+//
+//  Created by Tushar Kolhe on 15/04/18.
+//  Copyright © 2018 Tushar Kolhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TKTableViewWIthSelectionBtn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TKTableViewWIthSelectionBtn
+//
+//  Created by Tushar Kolhe on 15/04/18.
+//  Copyright © 2018 Tushar Kolhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
